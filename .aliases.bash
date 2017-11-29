1d68491c0a9b2d13cb403c54322174ed8f1ef0b6
Frederic Barthelemy <git@fbartho.com>
2017-11-29 12:51:26 -0800

    misc bash alias updates

--- .aliases.bash
+++ .aliases.bash
@@ -1,18 +1,56 @@
 source "/Volumes/secure-dotfiles/work_aliases.sh"
 source "/Volumes/secure-dotfiles/other_aliases.sh"
+alias resource="source ~/.profile"
 
 alias open="open2"
 alias up="svn up"
 alias st="svn st"
 alias octave="exec '/Applications/Octave.app/Contents/Resources/bin/octave'"
-alias restartsound="sudo kill -9 `ps ax|grep 'coreaudio[a-z]' |awk '{print $1}'`"
+
 alias lock="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend"
+alias saferestart="osascript -e 'tell app \"System Events\" to restart'"
+alias safeshutdown="osascript -e 'tell app \"System Events\" to shut down'"
+alias sleepnow="pmset sleepnow"
+alias displaysleepnow="pmset displaysleepnow"
+
+# Get the system & xcode versions!
+alias versions="{ sw_vers; echo "---"; xcodebuild -version; }"
 
 alias rmsvn="find ./ -name .svn -exec rm -rf {} \;"
 alias rmnonsvn="svn status | grep ? | awk '{print $2}' | xargs rm -rf"
 
-#alias xcp="open *.xc*"
-alias xcp="if [ \"$(ls *.xcworkspace 2> /dev/null | wc -l)\" != \"       0\" ]; then    open *.xcworkspace; else    open *.xcodeproj; fi"
+# Open Xcodeworkspace but fallback to xcodeproj
+# alias xcp="open *.xc*"
+# alias xcp="if [ \"$(ls *.xcworkspace 2> /dev/null | wc -l)\" != \"       0\" ]; then    open *.xcworkspace; else    open *.xcodeproj; fi"
+openx() {
+    fileToOpen='';
+    for file in `find . -maxdepth 1 -name *.xcworkspace`; do
+        fileToOpen=$file
+    done
+
+    if [ -n "$fileToOpen" ]
+    then
+        echo $fileToOpen
+        open $fileToOpen
+    else
+        for file in `find . -maxdepth 1 -name *.xcodeproj`; do
+            fileToOpen=$file
+        done
+
+        if [ -n "$fileToOpen" ]
+        then
+            echo $fileToOpen
+            open $fileToOpen
+        else
+            echo "No xcode files to open."
+        fi
+    fi
+}
+
+# Shorthand version of "openx", use "xcp" instead.
+alias xcp="openx"
+
+alias podxcp="pod install && xcp"
 
 alias esp="open -a Espresso "
 alias na="rm -rf build/Debug/*.app"
@@ -38,7 +76,12 @@ alias broforce="sudo /Users/fbarthelemy/Library/Application\ Support/Steam/Steam
 alias pa="sudo ~/Library/Application\ Support/Steam/SteamApps/common/Planetary\ Annihilation/PA.app/Contents/MacOS/PA"
 
 alias fix_audio="sudo kill -9 `ps ax|grep 'coreaudio[a-z]' | awk '{print $1}'`"
+alias restartsound="fix_audio"
 
 # Removes duplicated lines keeping the first instance of a line.
 alias uniq_inplace="awk '!x[\$0]++'"
 alias uniq_inplace_latest="tail -r | uniq_inplace | tail -r"
+
+# NodeJS Aliases
+yarn-upgrade="rm yarn.lock && yarn"
+
