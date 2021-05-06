7b9917b374162370a343d49bceafe2fcde26e354
Frederic Barthelemy <git@fbartho.com>
2021-05-05 17:42:09 -0700

    Misc Aliases

--- .aliases.bash
+++ .aliases.bash
@@ -13,6 +13,8 @@ alias safeshutdown="osascript -e 'tell app \"System Events\" to shut down'"
 alias sleepnow="pmset sleepnow"
 alias displaysleepnow="pmset displaysleepnow"
 
+alias nvm_upgrade_stable="nvm install stable --reinstall-packages-from=$(nvm current)"
+
 alias timemachine_info="sudo tmutil listbackups"
 alias timemachine_start="sudo tmutil startbackup"
 alias timemachine_stop="sudo tmutil stopbackup"
@@ -30,6 +32,7 @@ alias simshot="xcrun simctl io booted screenshot" # output-path
 alias simaddmedia="xcrun simctl addmedia booted" # path-to-media
 alias simopenurl="xcrun simctl openurl booted" # url
 alias simappcontainer="xcrun simctl get_app_container booted" # com.bundle.identifier
+alias lsregister=/System/Library/Frameworks/CoreServices.framework/Versions/A/Frameworks/LaunchServices.framework/Versions/A/Support/lsregister
 
 # Open Xcodeworkspace but fallback to xcodeproj
 # alias xcp="open *.xc*"
